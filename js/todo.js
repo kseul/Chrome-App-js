db59808316464b0cc65ee1c1ae0a405bf85d386b
seul <kseul06@gmail.com>
2022-01-19 11:01:53 +0900

    todo save

--- js/todo.js
+++ js/todo.js
@@ -2,7 +2,7 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.querySelector("#todo-list");
 const toDoInput = document.querySelector("#todo-form input");
 
-const toDos = [];
+let toDos = [];
 const TODOS_KEY = "todos";
 
 function saveToDos() {
@@ -14,33 +14,36 @@ function deleteToDo(e) {
   li1.remove();
 }
 
-function paintInto(newTodo) {
+// 화면에 표시, 버튼 이벤트리스너(click)
+function paintToDo(newTodo) {
   const li1 = document.createElement("li");
   const span1 = document.createElement("span");
   span1.innerText = newTodo;
   const Btn = document.createElement("button");
   Btn.innerText = "❌";
   Btn.addEventListener("click", deleteToDo);
+
   li1.appendChild(span1);
   li1.appendChild(Btn);
   toDoList.appendChild(li1);
 }
 
+// 폼 이벤트리스너(submit)
 function handleToDoSubmit(e) {
   e.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
   toDos.push(newTodo); // 배열에 넣음
-  paintInto(newTodo);
+  paintToDo(newTodo);
   saveToDos(); // 호출되는 시점에 newTodo는 배열에 있는 상태.
 }
-
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
+// localStorage세이브
 const savedToDos = localStorage.getItem(TODOS_KEY);
-if (saveToDos) {
-  // if(saveToDos !==null)
+if (savedToDos !== null) {
+  // )
   const parsedToDos = JSON.parse(savedToDos); // localStorage에서 온 string을 가지고 살아있는 JSobject로 변하게 함
-  console.log(parsedToDos);
-  parsedToDos.forEach((i) => console.log(`Hello ${i}`));
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
